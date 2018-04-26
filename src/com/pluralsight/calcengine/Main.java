ff61caf178ac9ecadacfdf870611d92b5c3efcb4
PredaG <38722442+PredaG@users.noreply.github.com>
2018-04-26 20:18:39 +0300

    Update Main.java

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -3,19 +3,7 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-  //      MathEquation testEquation = new MathEquation();
-//        testEquation.execute();
-  //     System.out.print("test=");
-//        System.out.println(testEquation.getResult());
-//
- //       double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-  //      double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-  //      char[] opCodes = {'d', 'a', 's', 'm'};
-   //     double[] results = new double[opCodes.length];
-       // double val1 = 100.0d;
-       // double val2 = 50.0d;
-       // double result;
-      //  double opCode = 'd';
+ 
         MathEquation[] equations = new MathEquation[4];
         equations[0] = new MathEquation('d', 100.d, 50.0d);
         equations[1] = new MathEquation('a', 25.0d, 92.0d);
@@ -29,13 +17,5 @@ public class Main {
            }
     }
 
-    /*
-public static MathEquation create(double leftVal, double rightVal, char opCode ){
-MathEquation equation = new MathEquation();
-equation.setLeftVal(leftVal);
-equation.setRightVal(rightVal);
-equation.setOpCode (opCode);
-return equation;
-}
-*/
+   
 }
